b0bfa8f97ed146f834d9518a1019e2fcaf050fe6
Sadia Haque <sadiahaque2017@gmail.com>
2025-03-06 11:08:14 +0600

    Add files via upload
    Additional jquery added

--- js/script.js
+++ js/script.js
@@ -88,3 +88,46 @@ $('.back-to-top').on('click', function(event) {
     }
     });  
 // Sticky-Header Part End
+
+//wow js Start
+	
+new WOW().init();
+
+// Wow Js End
+
+
+//	typed js
+	
+$(".typed").typed({
+    strings: [" Find Your Dream House Search By Area."],
+    // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+    stringsElement: null,
+    // typing speed
+    typeSpeed: 50,
+    // time before typing starts
+    startDelay: 1200,
+    // backspacing speed
+    backSpeed: 20,
+    // time before backspacing
+    backDelay: 500,
+    // loop
+    loop: true,
+    // false = infinite
+    loopCount: 5,
+    // show cursor
+    showCursor: false,
+    // character for cursor
+    cursorChar: "|",
+    // attribute to type (null == text)
+    attr: null,
+    // either html or text
+    contentType: 'html',
+    // call when done callback function
+    callback: function() {},
+    // starting callback function before each string
+    preStringTyped: function() {},
+    //callback for every typed string
+    onStringTyped: function() {},
+    // callback for reset
+    resetCallback: function() {}
+});
